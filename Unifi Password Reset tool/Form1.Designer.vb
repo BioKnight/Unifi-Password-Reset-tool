9cd47a210c5d5e78893d76186a47afcecb3fed55
BioKnight <randycmiller1981@gmail.com>
2022-04-06 12:25:18 -0400

    Functional version 1

--- Unifi Password Reset tool/Form1.Designer.vb
+++ Unifi Password Reset tool/Form1.Designer.vb
@@ -22,10 +22,115 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.lstAccounts = New System.Windows.Forms.ListBox()
+        Me.lblUsername = New System.Windows.Forms.Label()
+        Me.txtUsername = New System.Windows.Forms.TextBox()
+        Me.txtEmail = New System.Windows.Forms.TextBox()
+        Me.lblEmail = New System.Windows.Forms.Label()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.lblPassword = New System.Windows.Forms.Label()
+        Me.btn_Reset = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lstAccounts
+        '
+        Me.lstAccounts.FormattingEnabled = True
+        Me.lstAccounts.ItemHeight = 15
+        Me.lstAccounts.Location = New System.Drawing.Point(12, 12)
+        Me.lstAccounts.Name = "lstAccounts"
+        Me.lstAccounts.Size = New System.Drawing.Size(120, 94)
+        Me.lstAccounts.TabIndex = 0
+        '
+        'lblUsername
+        '
+        Me.lblUsername.AutoSize = True
+        Me.lblUsername.Location = New System.Drawing.Point(138, 12)
+        Me.lblUsername.Name = "lblUsername"
+        Me.lblUsername.Size = New System.Drawing.Size(60, 15)
+        Me.lblUsername.TabIndex = 1
+        Me.lblUsername.Text = "Username"
+        '
+        'txtUsername
+        '
+        Me.txtUsername.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtUsername.Location = New System.Drawing.Point(138, 30)
+        Me.txtUsername.Name = "txtUsername"
+        Me.txtUsername.ReadOnly = True
+        Me.txtUsername.Size = New System.Drawing.Size(60, 16)
+        Me.txtUsername.TabIndex = 2
+        '
+        'txtEmail
+        '
+        Me.txtEmail.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtEmail.Location = New System.Drawing.Point(204, 30)
+        Me.txtEmail.Name = "txtEmail"
+        Me.txtEmail.ReadOnly = True
+        Me.txtEmail.Size = New System.Drawing.Size(206, 16)
+        Me.txtEmail.TabIndex = 4
+        '
+        'lblEmail
+        '
+        Me.lblEmail.AutoSize = True
+        Me.lblEmail.Location = New System.Drawing.Point(204, 12)
+        Me.lblEmail.Name = "lblEmail"
+        Me.lblEmail.Size = New System.Drawing.Size(36, 15)
+        Me.lblEmail.TabIndex = 3
+        Me.lblEmail.Text = "Email"
+        '
+        'txtPassword
+        '
+        Me.txtPassword.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtPassword.Location = New System.Drawing.Point(138, 83)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.ReadOnly = True
+        Me.txtPassword.Size = New System.Drawing.Size(272, 16)
+        Me.txtPassword.TabIndex = 6
+        Me.txtPassword.Visible = False
+        '
+        'lblPassword
+        '
+        Me.lblPassword.AutoSize = True
+        Me.lblPassword.Location = New System.Drawing.Point(138, 65)
+        Me.lblPassword.Name = "lblPassword"
+        Me.lblPassword.Size = New System.Drawing.Size(57, 15)
+        Me.lblPassword.TabIndex = 5
+        Me.lblPassword.Text = "Password"
+        '
+        'btn_Reset
+        '
+        Me.btn_Reset.Location = New System.Drawing.Point(220, 83)
+        Me.btn_Reset.Name = "btn_Reset"
+        Me.btn_Reset.Size = New System.Drawing.Size(75, 23)
+        Me.btn_Reset.TabIndex = 7
+        Me.btn_Reset.Text = "Reset"
+        Me.btn_Reset.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(419, 118)
+        Me.Controls.Add(Me.btn_Reset)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.lblPassword)
+        Me.Controls.Add(Me.txtEmail)
+        Me.Controls.Add(Me.lblEmail)
+        Me.Controls.Add(Me.txtUsername)
+        Me.Controls.Add(Me.lblUsername)
+        Me.Controls.Add(Me.lstAccounts)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents lstAccounts As ListBox
+    Friend WithEvents lblUsername As Label
+    Friend WithEvents txtUsername As TextBox
+    Friend WithEvents txtEmail As TextBox
+    Friend WithEvents lblEmail As Label
+    Friend WithEvents txtPassword As TextBox
+    Friend WithEvents lblPassword As Label
+    Friend WithEvents btn_Reset As Button
 End Class
